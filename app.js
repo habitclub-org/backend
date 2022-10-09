b85dc6156eac42cd44ff07adba08f4b2de306d82
soheon-lee <lsheon93@gmail.com>
2022-10-09 13:40:45 +0900

    Add: swagger api doc added

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 import express from "express";
 import cors from "cors";
 import router from "./routers";
+import swaggerJsdoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express"
+import swaggerDocument from "./swagger.json"
 
 const createApp = () => {
   const app = express();
 
   app.use(express.json());
+
+	app.use(
+		"/api-docs",
+		swaggerUi.serve,
+		swaggerUi.setup(swaggerDocument, { explorer: true })
+	)
+
+
+
   app.use(router);
 	app.use(cors());
 
